7680c3f419df45d273e74fc4e6816a9dc9b2b89d
jamesandrewpohadi <32510504+jamesandrewpohadi@users.noreply.github.com>
2017-11-28 20:43:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-def filterRequest(re):
+def filRequest(re):
     if req.get("result").get("action") != "yahooWeatherForecast":
     res = processRequest(req)
     return res
