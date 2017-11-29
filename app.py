29d6b8361f8f37106fbf449ac5e1d1996b4cd9dc
jamesandrewpohadi <32510504+jamesandrewpohadi@users.noreply.github.com>
2017-11-29 13:50:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = proceRequest(req)
+    res = processRequest(req)
 
     res = json.dumps(res, indent=4)
     # print(res)
