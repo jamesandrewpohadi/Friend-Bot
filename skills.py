c5443f0479a6bcbc23a31853c1422bb7445823d9
jamesandrewpohadi <32510504+jamesandrewpohadi@users.noreply.github.com>
2018-02-06 21:18:48 +0800

    Update skills.py

--- skills.py
+++ skills.py
@@ -1,8 +1,23 @@
-def mod(a,b):
-	res = a % b
-	text = str(a) + " mod " + str(b) + " = " + str(res)
+def euler(req):
+	n = int(req.get("result").get("parameters").get("number"))
+    x = 0
+    for i in range(1,n):
+        for b in range(2,i+1):
+            if i%b == n%b == 0:
+                x = x + 1
+                break
+    res = n-x-1
 	return {
-		"speech": text
+		"speech": "euler({}) = {}".format(n,res)
+	}
+
+
+def mod(req):
+	a = int(req.get("result").get("parameters").get("number"))
+	b = int(req.get("result").get("parameters").get("number1"))
+	res = a%b
+	return {
+		"speech": str(a) + " mod " + str(b) + " = " + str(res)
 	}
 
 def processP_Check(req):
