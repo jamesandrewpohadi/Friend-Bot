3b0697066bbafc66109fee2037719c4f9f3600a2
jamesandrewpohadi <32510504+jamesandrewpohadi@users.noreply.github.com>
2018-02-06 20:26:15 +0800

    Update skills.py

--- skills.py
+++ skills.py
@@ -5,6 +5,19 @@ def mod(a,b):
 		"speech": text
 	}
 
-def primeFactor(n):
-	pass
+def processP_Check(req):
+    n = int(req.get("result").get("parameters").get("number"))
+    r = int(n**(1/2))
+    if n < 2:
+        return {
+            "speech": str(n) + " is not a prime number!" 
+        }
+    for i in range(2,r+1):
+        if n % i == 0:
+            return {
+                "speech": str(n) + " is not a prime number!"
+            }
+    return {
+        "speech": str(n) + " is a prime number!"
+    }
 	
